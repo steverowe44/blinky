4917b57922b489154d7f017b327f950b62815299
Stephen Rowe <roweredblue@gmail.com>
2026-03-07 23:31:25 +0000

    added overlay file for the 52840 - this enables gpiote, which is disabled by default on the device tree. Also, auto formatting main.c for some reason

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@
 #include <zephyr/drivers/gpio.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#define SLEEP_TIME_MS 4000
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
@@ -25,18 +25,22 @@ int main(void)
 	int ret;
 	bool led_state = true;
 
-	if (!gpio_is_ready_dt(&led)) {
+	if (!gpio_is_ready_dt(&led))
+	{
 		return 0;
 	}
 
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
+	if (ret < 0)
+	{
 		return 0;
 	}
 
-	while (1) {
+	while (1)
+	{
 		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
+		if (ret < 0)
+		{
 			return 0;
 		}
 
