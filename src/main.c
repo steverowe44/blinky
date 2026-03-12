52f2a7adc8410e67a1a820ccb035dd612c82c690
Stephen Rowe <roweredblue@gmail.com>
2026-03-12 21:40:12 +0000

    read button, switches led state

--- src/main.c
+++ src/main.c
@@ -9,16 +9,23 @@
 #include <zephyr/drivers/gpio.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS 4000
+#define SLEEP_TIME_MS 1000
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+#define BUTTON0_NODE DT_NODELABEL(button0)
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(BUTTON0_NODE, gpios);
+
+void pin_isr(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	gpio_pin_toggle_dt(&led);
+}
 
 int main(void)
 {
@@ -31,6 +38,23 @@ int main(void)
 	}
 
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0)
+		return 0;
+
+	// button config
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret < 0)
+		return 0;
+
+	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret < 0)
+		return 0;
+
+	// set interrupt for button
+	static struct gpio_callback pin_cb_data;
+	gpio_init_callback(&pin_cb_data, pin_isr, BIT(button.pin));
+	gpio_add_callback(button.port, &pin_cb_data);
+
 	if (ret < 0)
 	{
 		return 0;
