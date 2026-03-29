20fd52f40e5d5d5166fd8581c2041b4de5db61d2
Stephen Rowe <roweredblue@gmail.com>
2026-03-29 18:58:32 +0100

    now button X turns on led X.

--- src/main.c
+++ src/main.c
@@ -10,8 +10,6 @@
 
 LOG_MODULE_REGISTER(blinky, LOG_LEVEL_NONE);
 
-#define PULSE_TIME_MS 150
-
 static const struct gpio_dt_spec leds[] = {
 	GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios),
 	GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios),
@@ -26,14 +24,13 @@ static const struct gpio_dt_spec buttons[] = {
 	GPIO_DT_SPEC_GET(DT_NODELABEL(button3), gpios),
 };
 
-K_SEM_DEFINE(button_sem, 0, 1);
-
 void pin_isr(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
-	k_sem_give(&button_sem);
-	printk("Gave sem!\n");
-	LOG_INF("Test log info");
-	LOG_ERR("A log message in Error level!");
+	for (int i = 0; i < ARRAY_SIZE(buttons); i++)
+	{
+		if (pins & BIT(buttons[i].pin))
+			gpio_pin_set_dt(&leds[i], gpio_pin_get_dt(&buttons[i]));
+	}
 }
 
 int main(void)
@@ -57,25 +54,15 @@ int main(void)
 		ret = gpio_pin_configure_dt(&buttons[i], GPIO_INPUT);
 		if (ret < 0)
 			return 0;
-		ret = gpio_pin_interrupt_configure_dt(&buttons[i], GPIO_INT_EDGE_TO_ACTIVE);
+		ret = gpio_pin_interrupt_configure_dt(&buttons[i], GPIO_INT_EDGE_BOTH);
 		if (ret < 0)
 			return 0;
-
 		gpio_init_callback(&pin_cb_data[i], pin_isr, BIT(buttons[i].pin));
 		gpio_add_callback(buttons[i].port, &pin_cb_data[i]);
 	}
 
 	while (1)
-	{
-		k_sem_take(&button_sem, K_FOREVER);
-
-		for (int i = 0; i < ARRAY_SIZE(leds); i++)
-		{
-			gpio_pin_set_dt(&leds[i], 1);
-			k_msleep(PULSE_TIME_MS);
-			gpio_pin_set_dt(&leds[i], 0);
-		}
-	}
+		k_sleep(K_FOREVER);
 
 	return 0;
 }
